803efffe7cfabdf318ed4506e2ff46e0747225b3
Zachary <zachary.preator@gmail.com>
2021-03-02 21:16:59 -0700

    Added popular image

--- groupme.py
+++ groupme.py
@@ -28,11 +28,14 @@ def getMessages(group):
     messages_df = pd.DataFrame.from_dict(messageData, orient='columns')
     messages_df = messages_df.append(data).sort_values(by='created_at')
     messages_df = messages_df.drop_duplicates(['id'])
+    messages_df = convertAttachments(messages_df)
+    messages_df = setFavNum(messages_df)
     # print(len(messages_df))
     messages_df.to_csv('data.csv')
     return messages_df
 
 def getAllMessages(group):
+    print('Getting all messages, this will take a minute...')
     messageData = []
     for message in group.messages.list_all():
         messageData.append(message.data)
@@ -40,3 +43,32 @@ def getAllMessages(group):
     messages_df = pd.DataFrame.from_dict(messageData, orient='columns')
     messages_df.to_csv('data.csv')
     return messages_df
+
+def convertAttachments(messages_df):
+    attachments = messages_df['attachments'].to_list()
+    newList = []
+    for x in attachments:
+        try:
+            newList.append(eval(x))
+        except:
+            newList.append(x)
+    messages_df['attachments'] = newList
+    return messages_df
+
+def setFavNum(messages_df):
+    favorites = messages_df['favorited_by'].to_list()
+    fav_num = []
+    for x in favorites:
+        try:
+            fav_num.append(len(eval(x)))
+        except:
+            fav_num.append(len(x))
+    messages_df['fav_num'] = fav_num
+    return messages_df
+
+def getMostLikedImage(messages_df):
+    # df = getMessages(getGroup())
+    attach_df = messages_df[messages_df['attachments'].map(len) > 0]
+    attach_df = attach_df.sort_values(by='fav_num', ascending=False)
+    # print(attach_df['favorited_by'].head())
+    return attach_df.iloc[0]
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 import io
 import base64
-from groupme import getGroup, getMessages
+from groupme import getGroup, getMessages, getMostLikedImage
 
 app = Flask(__name__)
 
@@ -12,7 +12,16 @@ def home():
     group = getGroup()
     messages = getMessages(group)
     length = len(messages)
-    return render_template('home.html', title='Large Fry Larry\'s', total_messages=length)
+    row = getMostLikedImage(messages)
+    url = row['attachments'][0]['url']
+    user_best_image = row['name']
+    user_best_image_url = row['avatar_url']
+    return render_template('home.html', 
+                           title='Large Fry Larry\'s', 
+                           total_messages=length,
+                           url=url,
+                           user_best_image=user_best_image,
+                           user_best_image_url=user_best_image_url)
 
 
 if __name__ == "__main__":
