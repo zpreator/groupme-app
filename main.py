ce0227657b40f49777297d5daa990d03005c7e79
Zachary <zachary.preator@gmail.com>
2021-03-03 16:09:04 -0700

    oops

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from flask import Flask, render_template
 import io
 import base64
 from groupme import *
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
-@app.route('/')
+class memeForm():
+    num = IntegerField('Minimum likes?', validators=[DataRequired()])
+    name = StringField('Filter by username, or leave blank for all')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
     """Home page of app with form"""
     group = getGroup()
@@ -18,13 +24,15 @@ def home():
     user_best_image_url = row['avatar_url']
     popular_df = getMostPopular(messages)
     image = getPopularityPlot(popular_df)
+    mForm = memeForm()
     return render_template('home.html', 
                            title='Large Fry Larry\'s', 
                            total_messages=length,
                            url=url,
                            user_best_image=user_best_image,
                            user_best_image_url=user_best_image_url,
-                           most_popular_image=image)
+                           most_popular_image=image,
+                           memeForm = mForm)
 
 
 if __name__ == "__main__":
