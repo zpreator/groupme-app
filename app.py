a30e76da64a9ba800dd77e71888b13258dc8f74a
Zachary <zachary.preator@gmail.com>
2021-03-05 18:35:23 -0700

    Added login from google

---
+++ app.py
@@ -0,0 +1,205 @@
+"""Initialize Flask Application."""
+#Python Standard libraries
+import io
+import base64
+import os
+import json
+import sqlite3
+
+# Third-party libraries
+from flask import Flask, render_template, redirect, url_for, request, session
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+
+# Internal imports
+from groupme import *
+from db import init_db_command
+from user import User
+
+# Configuration
+# GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_ID="844834903360-c5iu12hejg576jkvo4oo6vvhkts1g65m.apps.googleusercontent.com"
+# GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_CLIENT_SECRET = 'gF3kHGUvTuNh3olDRFLRKmZh'
+GOOGLE_DISCOVERY_URL = ("https://accounts.google.com/.well-known/openid-configuration")
+
+# Flask app setup
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+bootstrap = Bootstrap(app)
+
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return "You must be logged in to access this content.", 403
+
+
+# Naive database setup
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
+
+# OAuth2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if current_user.is_authenticated:
+        # return (
+        #     "<p>Hello, {}! You're logged in! Email: {}</p>"
+        #     "<div><p>Google Profile Picture:</p>"
+        #     '<img src="{}" alt="Google profile pic"></img></div>'
+        #     '<a class="button" href="/logout">Logout</a>'.format(
+        #         current_user.name, current_user.email, current_user.profile_pic
+        #     )
+        # )
+        return showIndex()
+    else:
+        return render_template('login.html')
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    return redirect(request_uri)
+    # return render_template('login.html', link=request_uri)
+
+
+@app.route("/login/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code,
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that we have tokens (yay) let's find and hit URL
+    # from Google that gives you user's profile information,
+    # including their Google Profile Image and Email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # We want to make sure their email is verified.
+    # The user authenticated with Google, authorized our
+    # app, and now we've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # Create a user in our db with the information provided
+    # by Google
+    user = User(
+        id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+    )
+
+    # Doesn't exist? Add to database
+    if not User.get(unique_id):
+        User.create(unique_id, users_name, users_email, picture)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("index"))
+
+
+@app.route("/logout", methods=['POST', 'GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+class memeForm(FlaskForm):
+    likes = IntegerField('likes', validators=[DataRequired()])
+
+def showIndex():
+    group = getGroup()
+    messages = getMessages(group)
+    length = len(messages)
+    url, user_best_image, user_best_image_url = getMostLikedImage(messages)
+    setFavNum(messages)
+    popular_df = getMostPopular(messages)
+    total_posts_image = getTotalPostsPlot(messages)
+    most_popular_image = getPopularityPlot(popular_df)
+    likes_per_post_image = getLikesPerPost(messages)
+    form = memeForm()
+    rand_url, user_rand_image, user_rand_image_url = getRandomMeme(messages, form.likes.data)
+    return render_template('index.html', 
+                        title='Large Fry Larry\'s', 
+                        total_messages=length,
+                        url=url,
+                        user_best_image=user_best_image,
+                        user_best_image_url=user_best_image_url,
+                        total_posts_image=total_posts_image,
+                        most_popular_image=most_popular_image,
+                        likes_per_post_image=likes_per_post_image,
+                        form = form,
+                        rand_url=rand_url,
+                        user_rand_image=user_rand_image,
+                        user_rand_image_url=user_rand_image_url)
+
+if __name__ == "__main__":
+    app.run(ssl_context="adhoc")
